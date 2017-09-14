c5545bcadca3595fc5c5df241ca8dd8da0f0e562
Christian Kniep <christian@qnib.org>
2017-09-14 12:30:05 +0200

    clean out the metrics-parsing from this filter

--- plugin.go
+++ plugin.go
@@ -2,22 +2,18 @@ package qfilter_grok
 
 import (
 	"fmt"
+	"path/filepath"
+	"os"
 	"reflect"
-	"strconv"
 	"strings"
 	"sync"
-	"time"
 	"github.com/vjeantet/grok"
 	"github.com/zpatrick/go-config"
 
 	"github.com/qframe/types/plugin"
 	"github.com/qframe/types/qchannel"
 	"github.com/qframe/types/messages"
-	"github.com/qframe/types/metrics"
 	"github.com/deckarep/golang-set"
-
-	"path/filepath"
-	"os"
 )
 
 const (
@@ -31,7 +27,6 @@ type Plugin struct {
 	*qtypes_plugin.Plugin
 	mu 				sync.Mutex
 	grok    		*grok.Grok
-	expectMetric	bool
 	expectJson 		bool
 	isBuffered 		bool
 	pattern 		string
@@ -45,6 +40,7 @@ func (p *Plugin) GetOverwriteKeys() []string {
 	return strings.Split(inStr, ",")
 }
 
+// New creates a new instance of the Plugin.
 func New(qChan qtypes_qchannel.QChan, cfg *config.Config, name string) (p Plugin, err error) {
 	p = Plugin{
 		Plugin: qtypes_plugin.NewNamedPlugin(qChan, cfg, pluginTyp, pluginPkg,  name, version),
@@ -52,6 +48,7 @@ func New(qChan qtypes_qchannel.QChan, cfg *config.Config, name string) (p Plugin
 	return p, err
 }
 
+// Match matches the pattern against a string and returns the values extracted.
 func (p *Plugin) Match(str string) (map[string]string, bool) {
 	match := true
 	val, _ := p.grok.Parse(p.pattern, str)
@@ -67,10 +64,12 @@ func (p *Plugin) Match(str string) (map[string]string, bool) {
 	return val, match
 }
 
+// GetPattern returns the pattern used.
 func (p *Plugin) GetPattern() string {
 	return p.pattern
 }
 
+// InitGrok() kicks of grok and adds patterns to the grok-instance.
 func (p *Plugin) InitGrok() {
 	p.grok, _ = grok.New()
 	var err error
@@ -109,40 +108,6 @@ func (p *Plugin) InitGrok() {
 	}
 }
 
-
-func (p *Plugin) sendMetric(t time.Time, tags, kv map[string]string) {
-	key, kOk := kv["key"]
-	if !kOk {
-		p.Log("trace", "Expect to match metric, but group 'key' could not be found in pattern")
-		return
-	}
-	valueStr, vOk := kv["value"]
-	if !vOk {
-		p.Log("trace", "Expect to match metric, but group 'value' could not be found in pattern")
-		return
-	}
-	val, err := strconv.ParseFloat(valueStr,10)
-	if err != nil {
-		p.Log("trace", fmt.Sprintf("String '%s' could not be parsed to become a float", valueStr))
-		return
-	}
-	tagsStr, tOk := kv["tags"]
-	dimensions := tags
-	if tOk {
-		for _, t := range strings.Split(tagsStr, " ") {
-			sl := strings.Split(t, "=")
-			if len(sl) != 2 {
-				p. Log("trace", fmt.Sprintf("Failed to parse tag '%s' into key=val", t))
-				return
-			}
-			dimensions[sl[0]] = sl[1]
-		}
-	}
-	m := qtypes_metrics.NewExt(p.Name, key, qtypes_metrics.Gauge, val, dimensions, t, p.isBuffered)
-	p.Log("debug", fmt.Sprintf("Send metric: Key:%s Val:%.1f Dims:%v", m.Name, m.Value, m.Dimensions))
-	p.QChan.Data.Send(m)
-}
-
 // Lock locks the plugins' mutex.
 func (p *Plugin) Lock() {
 	p.mu.Lock()
@@ -157,13 +122,14 @@ func (p *Plugin) Unlock() {
 func (p *Plugin) Run() {
 	p.Log("notice", fmt.Sprintf("Start grok filter v%s", p.Version))
 	p.InitGrok()
-	p.expectMetric = p.CfgBoolOr("expect-metric", false)
+	//p.expectMetric = p.CfgBoolOr("expect-metric", false)
 	p.expectJson = p.CfgBoolOr("expect-json", false)
-	p.isBuffered = p.CfgBoolOr("buffer-metric", false)
+	//p.isBuffered = p.CfgBoolOr("buffer-metric", false)
 	bg := p.QChan.Data.Join()
 	msgKey := p.CfgStringOr("overwrite-message-key", "")
 	for {
 		val := bg.Recv()
+		p.Log("trace", fmt.Sprintf("received %s", reflect.TypeOf(val)))
 		switch val.(type) {
 		case qtypes_messages.ContainerMessage:
 			cm := val.(qtypes_messages.ContainerMessage)
@@ -175,7 +141,7 @@ func (p *Plugin) Run() {
 			kv, cm.SourceSuccess = p.Match(cm.Message.Message)
 			if cm.SourceSuccess {
 				p.Log("debug", fmt.Sprintf("Matched pattern '%s'", p.pattern))
-				if p.expectMetric {
+				/*if p.expectMetric {
 					tags := cm.Tags
 					tags["container_id"] = cm.Container.ID
 					tags["image"] = cm.Container.Config.Image
@@ -183,7 +149,7 @@ func (p *Plugin) Run() {
 					tags["container_name"] = strings.TrimLeft(cm.Container.Name,"/")
 					p.sendMetric(cm.Time, tags, kv)
 					continue
-				}
+				}*/
 				if p.expectJson {
 					p.Lock()
 					cm.Message.ParseJsonMap(p.Plugin, kv)
@@ -207,26 +173,24 @@ func (p *Plugin) Run() {
 				continue
 			}
 			qm.AppendSource(p.Name)
-			var kv map[string]string
-			kv, qm.SourceSuccess = p.Match(qm.Message)
+			kv, SourceSuccess := p.Match(qm.Message)
+			qm.SourceSuccess = SourceSuccess
 			if qm.SourceSuccess {
 				p.Log("debug", fmt.Sprintf("Matched pattern '%s'", p.pattern))
-				if p.expectMetric {
-					p.sendMetric(qm.Time, qm.Tags, kv)
-					continue
-				}
 				if p.expectJson {
 					p.Lock()
 					qm.ParseJsonMap(p.Plugin, kv)
 					p.Unlock()
 				}
 				for k,v := range kv {
-					p.Log("debug", fmt.Sprintf("    %15s: %s", k,v ))
+					p.Log("trace", fmt.Sprintf("    %15s: %s", k,v ))
 					qm.Tags[k] = v
 					if msgKey == k {
 						qm.Message = v
 					}
 				}
+				p.QChan.Data.Send(qm)
+				continue
 			} else {
 				p.Log("debug", fmt.Sprintf("No match of '%s' for message '%s'", p.pattern, qm.Message))
 			}
